c980caeb27b62d729dbf6d399e438fa6dbd5b129
kathleenhung5 <kathleenhung5@gmail.com>
2019-04-08 16:36:51 -0700

    NA page added animals and js

--- Continents/NorthAmerica/northamerica.js
+++ Continents/NorthAmerica/northamerica.js
@@ -1,19 +1,44 @@
 var app = document.getElementById("app");
 var sun = document.querySelector(".sun");
+var landAni = document.querySelector(".land-animals"),
+    oceanAni = document.querySelector(".ocean-animals");
 var landBut = document.getElementById("land"),
-    landBut = document.getElementById("ocean");
+    oceanBut = document.getElementById("ocean");
 
-
-function goLand (){
+function goLand(){
     app.style.backgroundPosition = "left";
     sun.style.left = "70%";
-    land.style.opacity = "0";
-    ocean.style.opacity = "1";
+    landBut.style.opacity = "0";
+    oceanBut.style.opacity = "1";
+    oceanAni.style.left = "260%";
+    landAni.style.left = "10%";
 }
 
-function goOcean (){
+function goOcean(){
     app.style.backgroundPosition = "right";
     sun.style.left = "-5%";
-    ocean.style.opacity = "0";
-    land.style.opacity = "1";
+    oceanBut.style.opacity = "0";
+    landBut.style.opacity = "1";
+    landAni.style.left = "-200%";
+    oceanAni.style.left = "10%";
+}
+
+function goOtter(){
+    document.querySelector(".otter img").src = "Otter/Otter_SVGs/Otter.svg";
+    //window.location.href = "";
+}
+
+function goBeaver(){
+    document.querySelector(".beaver img").src = "Beaver/Beaver_SVGs/Beaver.svg";
+    //window.location.href = "";
+}
+
+function goOrca(){
+    document.querySelector(".orca img").src = "Orca/Orca_SVGs/Orca.svg";
+    //window.location.href = "";
+}
+
+function goJellyfish(){
+    document.querySelector(".jellyfish img").src = "Jellyfish/Jellyfish_SVGs/jellyfish.svg";
+    //window.location.href = "";
 }
