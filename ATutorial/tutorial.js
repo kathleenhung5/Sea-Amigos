3c74388cd784ac9838e76f9c62380c54adc3c341
kathleenhung5 <kathleenhung5@gmail.com>
2019-04-12 09:18:20 -0700

    edited tutorial NA

--- ATutorial/tutorial.js
+++ ATutorial/tutorial.js
@@ -1,5 +1,3 @@
-
-
 function tutorialOtter(){
    
     document.querySelector(".otter img").src = "../Continents/NorthAmerica/Otter/Otter_SVGs/Otter.svg";
@@ -9,7 +7,12 @@ function tutorialOtter(){
 function toNATutorial(){
    window.location.href = "tutorial-northamerica.html"; 
 }
-    
+  
+function moveToRight(){
+    document.querySelector(".talkbox-NA").style.left = "100%";
+    document.querySelector(".turtle-NA").style.opacity = "0";
+    document.querySelector("#ocean").style.opacity = "0";
+}
 
 
 
