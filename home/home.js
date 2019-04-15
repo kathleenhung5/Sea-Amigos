2b4cc7066684d137a2032387c737714b5e85eb33
Kathleen Hung <a01044903@se14l0124-03.ad.bcit.ca>
2019-04-15 14:55:51 -0700

    reorganized home and map

--- home/home.js
+++
@@ -1,7 +0,0 @@
-function toMap (){
-    window.location.href = "./Map/worldmap.html";
-}
-
-function toTutorial(){
-    window.location.href = "./ATutorial/tutorial-welcome.html";
-}
---
+++ js/home.js
@@ -0,0 +1,12 @@
+function toMap (){
+    document.querySelector(".map-page").style.left = "0px";
+    console.log("tomap");
+}
+
+function toTutorial(){
+    window.location.href = "./ATutorial/tutorial-welcome.html";
+}
+
+function toNA(){
+    window.location.href = "../Continents/NorthAmerica/northamerica.html";
+}
