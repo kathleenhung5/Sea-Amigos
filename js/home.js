76c99ce39786eb65d78bbf78569c757d167c4c3c
kathleenhung5 <kathleenhung5@gmail.com>
2019-05-01 21:39:41 -0700

    export quiz background

--- js/home.js
+++ js/home.js
@@ -859,6 +859,7 @@ function closePopup(){
 //------ Quiz Page --------//
 
 // -- Quiz Questions----
+// NORTH AMERICAN ANIMALS //
 var OtterQuiz = [
     {
         q:"What do I eat?",
@@ -894,6 +895,535 @@ var OtterQuiz = [
     }
 ]
 
+var BeaverQuiz = [
+    {
+        q:"What time of day do I work?",
+        a:3,
+        but1:"Morning",
+        but2:"Afternoon",
+        but3:"Evening",
+        but4:"Midnight"
+    },
+    {
+        q: "When do I slap my tail?",
+        a: 2,
+        but1: "When I'm excited",
+        but2: "When I'm in danger",
+        but3: "When I wake up",
+        but4: "Before I make a dam"
+    },
+    {
+        q: "How long can I stay under the water?",
+        a: 1,
+        but1: "15 minutes",
+        but2: "15 seconds",
+        but3: "15 hours",
+        but4: "15 milliseconds"
+    },
+    {    
+        q: "How do I make dams?",
+        a: 1,
+        but1: "I chew on trees",
+        but2: "I move rocks",
+        but3: "I slap mud in place",
+        but4: "I use leaves"
+    }
+]
+
+var JellyfishQuiz = [
+    {
+        q:"What organ am I missing?",
+        a:2,
+        but1:"Stomach",
+        but2:"Brain",
+        but3:"Intestines",
+        but4:"Nose"
+    },
+    {
+        q: "How big are the smallest jellyfish?",
+        a: 2,
+        but1: "The size of a marble",
+        but2: "The size of a peanut",
+        but3: "The size of your hand",
+        but4: "The size of an iPhone"
+    },
+    {
+        q: "Most jellyfish sting, but some jellyfish are also _________",
+        a: 1,
+        but1: "Poisonous",
+        but2: "Invisible",
+        but3: "Super fast",
+        but4: "Smelly"
+    },
+    {    
+        q: "What do we like to eat?",
+        a: 3,
+        but1:"<img src='img/northamerica/animal-page/Fruit.svg'/>",
+        but2:"<img src='img/northamerica/animal-page/Fish.svg'/>",
+        but3:"<img src='img/northamerica/animal-page/Plankton.svg'/>",
+        but4:"<img src='img/northamerica/animal-page/Seaweed.svg'/>"
+    }
+]
+
+var OrcaQuiz = [
+    {
+        q:"What another nickname for Orcas?",
+        a:1,
+        but1:"Killer Whales",
+        but2:"Dolphins",
+        but3:"Dark Whales",
+        but4:"Hunting Whales"
+    },
+    {
+        q: "Orcas don't have a sense of __________",
+        a: 3,
+        but1: "Touch",
+        but2: "Sight",
+        but3: "Smell",
+        but4: "Hearing"
+    },
+    {
+        q: "Orcas are a type of __________",
+        a: 4,
+        but1: "Fish",
+        but2: "Reptile",
+        but3: "Whale",
+        but4: "Dolphin"
+    },
+    {    
+        q: "Which senses help us hunt for food?",
+        a: 1,
+        but1: "Seeing and hearing",
+        but2: "Hearing and feeling",
+        but3: "Feeling and smelling",
+        but4: "Smelling and hearing"
+    }
+]
+
+// ANTARCTICA ANIMALS //
+var PenguinQuiz = [
+    {
+        q:"What season do we breed?",
+        a:3,
+        but1:"Summer",
+        but2:"Autumn",
+        but3:"Spring",
+        but4:"Winter"
+    },
+    {
+        q: "How far can we swim underwater?",
+        a: 4,
+        but1: "100 metres",
+        but2: "10 metres",
+        but3: "50 metres",
+        but4: "500 metres"
+    },
+    {
+        q: "How long can we stay underwater?",
+        a: 2,
+        but1: "10 minutes",
+        but2: "22 minutes",
+        but3: "5 minutes",
+        but4: "30 minutes"
+    },
+    {   
+        q: "Who takes care of the baby penguins?",
+        a: 1,
+        but1: "Mom and dad penguin",
+        but2: "Mom penguin only",
+        but3: "Dad penguin only",
+        but4: "They take care of themselves"
+    }
+]
+
+var SealQuiz = [
+    {
+        q:"Where do I spend most of my time?",
+        a:1,
+        but1:"Underwater",
+        but2:"On the ice",
+        but3:"On rocky shores",
+        but4:"In the snow"
+    },
+    {
+        q: "What colours do we see best?",
+        a: 3,
+        but1: "Red and green",
+        but2: "Purple and blue",
+        but3: "Blue and green",
+        but4: "Blue and red"
+    },
+    {
+        q: "Underwater, we have really good ___________",
+        a: 4,
+        but1: "Taste",
+        but2: "Smell",
+        but3: "Sight",
+        but4: "Hearing"
+    },
+    {    
+        q: "What keeps us warm in the cold?",
+        a: 2,
+        but1: "Fur",
+        but2: "Blubber",
+        but3: "Water",
+        but4: "Ice"
+    }
+]
+
+var WhaleQuiz = [
+    {
+        q:"What are groups of whales called?",
+        a:3,
+        but1:"Herd",
+        but2:"Gang",
+        but3:"Pods",
+        but4:"Colony"
+    },
+    {
+        q: "What is one of the noises I make when I talk to other whales?",
+        a: 2,
+        but1: "Yell",
+        but2: "Whistle",
+        but3: "Snort",
+        but4: "Mumble"
+    },
+    {
+        q: "Which animal is larger than a blue whale?",
+        a: 4,
+        but1: "Narwhals",
+        but2: "Giant squids",
+        but3: "Belgua whales",
+        but4: "None of them!"
+    },
+    {    
+        q: "What my favourite food?",
+        a: 3,
+        but1:"<img src='img/northamerica/animal-page/Fruit.svg'/>",
+        but2:"<img src='img/northamerica/animal-page/Fish.svg'/>",
+        but3:"<img src='img/northamerica/animal-page/Plankton.svg'/>",
+        but4:"<img src='img/northamerica/animal-page/Seaweed.svg'/>"
+    }
+]
+
+var SquidQuiz = [
+    {
+        q:"Which animal is related to the squid?",
+        a:2,
+        but1:"Jellyfish",
+        but2:"Octopus",
+        but3:"Sea Anenome",
+        but4:"Starfish"
+    },
+    {
+        q: "What do squids do when they are in danger?",
+        a: 2,
+        but1: "Use their tentacles",
+        but2: "Shoot ink",
+        but3: "Run away",
+        but4: "Stay still"
+    },
+    {
+        q: "How do squids hide from predators?",
+        a: 4,
+        but1: "They change colour",
+        but2: "They hide between rocks",
+        but3: "They hide with other animals",
+        but4: "They hide under the sand"
+    },
+    {    
+        q: "What happens when a squid lose an arm?",
+        a: 2,
+        but1:"It's gone forever",
+        but2:"It will grow back",
+        but3:"It will grow back with an extra arm",
+        but4:"They can't swim"
+    }
+]
+
+//NORTH POLE ANIMALS//
+var WalrusQuiz = [
+    {
+        q:"How long do walruses live?",
+        a:3,
+        but1:"50 years",
+        but2:"20 years",
+        but3:"40 years",
+        but4:"10 years"
+    },
+    {
+        q: "Walruses can weigh up to __________",
+        a: 1,
+        but1: "4,000 pounds",
+        but2: "100 pounds",
+        but3: "20,000 pounds",
+        but4: "10,000 pounds"
+    },
+    {
+        q: "What do walruses like to eat?",
+        a: 1,
+        but1:"<img src='img/northamerica/animal-page/Fruit.svg'/>",
+        but2:"<img src='img/northamerica/animal-page/Fish.svg'/>",
+        but3:"<img src='img/northamerica/animal-page/Plankton.svg'/>",
+        but4:"<img src='img/northamerica/animal-page/Seaweed.svg'/>"
+    },
+    {    
+        q: "What do walruses use their tusks for?",
+        a: 3,
+        but1:"To scratch each other's backs",
+        but2:"To climb rocks",
+        but3:"To hunt for food",
+        but4:"To cut ice"
+    }
+]
+
+var BearQuiz = [
+    {
+        q:"What colour are polar bears' fur?",
+        a:4,
+        but1:"Black",
+        but2:"Grey",
+        but3:"White",
+        but4:"Transparent"
+    },
+    {
+        q: "How long can polar bears swim for?",
+        a: 1,
+        but1: "Several hours",
+        but2: "Several days",
+        but3: "A few minutes",
+        but4: "A few hours"
+    },
+    {
+        q: "Where do polar bears usually look for food?",
+        a: 3,
+        but1:"Underwater",
+        but2:"On rocks",
+        but3:"On ice",
+        but4:"In the snow"
+    },
+    {    
+        q: "What colour is a polar bear's skin?",
+        a: 4,
+        but1:"White",
+        but2:"Brown",
+        but3:"Grey",
+        but4:"Black"
+    }
+]
+
+
+var NarwhalQuiz = [
+    {
+        q:"How long does a narwhal's tooth grow for?",
+        a:4,
+        but1:"10 years",
+        but2:"5 years",
+        but3:"Until they are old",
+        but4:"Their whole life"
+    },
+    {
+        q: "What colour does a narwhal's skin become?",
+        a: 3,
+        but1: "Black",
+        but2: "Pink",
+        but3: "White",
+        but4: "Blue"
+    },
+    {
+        q: "A narwhal's tusk is their ______ tooth",
+        a: 2,
+        but1:"Right",
+        but2:"Left",
+        but3:"Front",
+        but4:"Back"
+    },
+    {    
+        q: "A narwhal's tooth can grow up to ___ feet long.",
+        a: 1,
+        but1:"10",
+        but2:"5",
+        but3:"2",
+        but4:"15"
+    }
+]
+
+var BelugaQuiz = [
+    {
+        q:"What colour is our skin when we're born?",
+        a:1,
+        but1:"Grey",
+        but2:"White",
+        but3:"Blue",
+        but4:"Black"
+    },
+    {
+        q: "Name a noise I make when I talk to my friends.",
+        a: 3,
+        but1: "Yelling",
+        but2: "Clicking",
+        but3: "Squealing",
+        but4: "Burping"
+    },
+    {
+        q: "What is the bump on my head called?",
+        a: 3,
+        but1:"Brain",
+        but2:"Bump",
+        but3:"Melon",
+        but4:"Noggin"
+    },
+    {    
+        q: "What helps me find my way through water?",
+        a: 4,
+        but1:"Snapping",
+        but2:"Seeing",
+        but3:"Feeling",
+        but4:"Echolocation"
+    }
+]
+
+// AMAZON ANIMALS //
+var CrabQuiz = [
+    {
+        q:"What's another name for the Coconut crab?",
+        a:4,
+        but1:"Cococrab",
+        but2: "Hermit Crab",
+        but3: "Fruity Crab",
+        but4:"Robber Crab"
+    },
+    {
+        q: "Does the Coconut crab like living with others?",
+        a: 2,
+        but1: "Yes",
+        but2: "No",
+        but3: "Only with friends",
+        but4: "Only with family"
+    },
+    {
+        q: "What does the Coconut crab like to eat?",
+        a: 1,
+        but1:"<img src='img/northamerica/animal-page/Fruit.svg'/>",
+        but2:"<img src='img/northamerica/animal-page/Fish.svg'/>",
+        but3:"<img src='img/northamerica/animal-page/Plankton.svg'/>",
+        but4:"<img src='img/northamerica/animal-page/Seaweed.svg'/>"
+    },   
+    {
+        q: "What time of day does they like to hunt?",
+        a: 4,
+        but1:"Morning",
+        but2:"Evening",
+        but3:"Afternoon",
+        but4:"Night"
+    }
+]
+
+var CrocodileQuiz = [
+    {
+        q:"How many eggs to Crocodiles lay?",
+        a:3,
+        but1:"10-20",
+        but2: "30-40",
+        but3: "20-80",
+        but4:"50-100"
+    },
+    {
+        q: "Crocodiles can see very well in _________",
+        a: 2,
+        but1: "The day",
+        but2: "The water",
+        but3: "The fog",
+        but4: "The dark"
+    },
+    {
+        q: "How long do Crocodiles take care of their eggs?",
+        a: 1,
+        but1:"3 months",
+        but2:"6 months",
+        but3:"9 months",
+        but4:"12 months"
+    },
+    {
+        q: "Crocodiles _______ when they eat",
+        a: 4,
+        but1:"Chew",
+        but2:"Cry",
+        but3:"Breathe",
+        but4:"Close their eyes"
+    }
+]
+
+var ManateeQuiz = [
+    {
+        q:"Which animal is related to the manatee?",
+        a:3,
+        but1:"Cows",
+        but2: "Dolphins",
+        but3: "Elephants",
+        but4:"Seals"
+    },
+    {
+        q: "What is a nickname for the manatee?",
+        a: 4,
+        but1: "Amazon Seal",
+        but2: "Ocean dogs",
+        but3: "Dugong",
+        but4: "Sea Cows"
+    },
+    {
+        q: "What is the texture of a manatee's brain?",
+        a: 1,
+        but1:"Smooth",
+        but2:"Bumpy",
+        but3:"Rough",
+        but4:"Dry"
+    },
+    {
+        q: "What do manatees like to eat?",
+        a: 4,
+        but1:"<img src='img/northamerica/animal-page/Fruit.svg'/>",
+        but2:"<img src='img/northamerica/animal-page/Fish.svg'/>",
+        but3:"<img src='img/northamerica/animal-page/Plankton.svg'/>",
+        but4:"<img src='img/northamerica/animal-page/Seaweed.svg'/>"
+    }
+]
+
+var DolphinQuiz = [
+    {
+        q:"How many stomachs does the Amazonian River Dolphin have?",
+        a:2,
+        but1:"1",
+        but2: "2",
+        but3: "3",
+        but4:"4"
+    },
+    {
+        q: "How do dolphins talk?",
+        a: 1,
+        but1: "Whistling",
+        but2: "Yelling",
+        but3: "Laughing",
+        but4: "Pulsed Calls"
+    },
+    {
+        q: "How big is a dolphin's brain compared to a human brain?",
+        a: 3,
+        but1:"20% larger",
+        but2:"20% smaller",
+        but3:"40% larger",
+        but4:"40% smaller"
+    },
+    {
+        q: "Some legends say dolphins are ________",
+        a: 2,
+        but1:"Evil",
+        but2:"Magical",
+        but3:"Healers",
+        but4:"Mysterious"
+    }
+]
+
 
 //--- Quiz UI ----
 var qnum = 0,
@@ -919,56 +1449,55 @@ function quizContent(){
             if (pkg.quiz.otter.length >=3){
                 expert();
             } else {
+                qresult.style.display = "none";
                 pkg.quiz.otter = [];
                 save();
                 qnum = 0;
                 qna();
             }
-            
-           //checkq(pkg.quiz.otter); 
+        }
+        
+        if(pkg.animal == 2){
+            quizani.src = "img/northamerica/Beaver.svg";
+            if (pkg.quiz.beaver.length >=3){
+                expert();
+            } else {
+                qresult.style.display = "none";
+                pkg.quiz.beaver = [];
+                save();
+                qnum = 0;
+                qna();
+            }
+        }
+        
+        if(pkg.animal == 3){
+            quizani.src = "img/northamerica/jellyfish.svg";
+            if (pkg.quiz.jellyfish.length >=3){
+                expert();
+            } else {
+                qresult.style.display = "none";
+                pkg.quiz.jellyfish = [];
+                save();
+                qnum = 0;
+                qna();
+            }
+        }
+        
+        if(pkg.animal == 4){
+            quizani.src = "img/northamerica/Orca.svg";
+            if (pkg.quiz.orca.length >=3){
+                expert();
+            } else {
+                qresult.style.display = "none";
+                pkg.quiz.orca = [];
+                save();
+                qnum = 0;
+                qna();
+            }
         }
     }
 }
 
-//function checkq(x){
-//check if all questions are completed
-//    var zero = 0,
-//        one = 0,
-//        two = 0,
-//        three = 0;
-//    for (i=0;i<x.length;i++){
-//        if(x[i] == 0){
-//            zero = 1;
-//        } else if (x[i] == 1){
-//            one = 1;
-//        } else if (x[i] == 2){
-//            two = 1;
-//        } else if (x[i] == 3){
-//            three = 1;
-//        }
-//    }
-//    
-//    if (zero==1 && one==1 && two==1 && three==1){
-//        expert();
-//    } else {
-//        var y = 0;
-//        while(x.includes(y) == true){
-//            y=y+1;
-//        }
-//        qnum = y;
-//        qna();
-//    }  
-//    if(x.length >= 3){
-//        expert();
-//    } else {
-//        var y = 0;
-//        while(x.includes(y) == true){
-//            y=y+1;
-//        }
-//        qnum = y;
-//        qna();
-//    }
-//}
 
 function qna(){    
     if (pkg.continent == "NA"){
@@ -980,13 +1509,25 @@ function qna(){
             but4.innerHTML = OtterQuiz[qnum].but4;  
         }
         if (pkg.animal == 2){
-            quizani.src = "img/northamerica/Beaver.svg";
+            quizq.innerHTML = BeaverQuiz[qnum].q;
+            but1.innerHTML = BeaverQuiz[qnum].but1;
+            but2.innerHTML = BeaverQuiz[qnum].but2;
+            but3.innerHTML = BeaverQuiz[qnum].but3;
+            but4.innerHTML = BeaverQuiz[qnum].but4;  
         }
         if (pkg.animal == 3){
-            quizani.src = "img/northamerica/jellyfish.svg";
+            quizq.innerHTML = JellyfishQuiz[qnum].q;
+            but1.innerHTML = JellyfishQuiz[qnum].but1;
+            but2.innerHTML = JellyfishQuiz[qnum].but2;
+            but3.innerHTML = JellyfishQuiz[qnum].but3;
+            but4.innerHTML = JellyfishQuiz[qnum].but4;  
         }
         if (pkg.animal == 4){
-            quizani.src = "img/northamerica/Orca.svg";
+            quizq.innerHTML = OrcaQuiz[qnum].q;
+            but1.innerHTML = OrcaQuiz[qnum].but1;
+            but2.innerHTML = OrcaQuiz[qnum].but2;
+            but3.innerHTML = OrcaQuiz[qnum].but3;
+            but4.innerHTML = OrcaQuiz[qnum].but4;  
         }
     }
 
@@ -1005,7 +1546,6 @@ function checka (butnum){
                 } else {
                     qnum = qnum + 1;
                 }
-                //nextq(pkg.quiz.otter);
             } else {
                 console.log('wrong');
                 if (qnum == 3){
@@ -1013,7 +1553,69 @@ function checka (butnum){
                 } else {
                     qnum = qnum + 1;
                 }
-                //nextq(pkg.quiz.otter);
+            }
+        }
+        
+         if (pkg.animal == 2){
+            if (butnum == BeaverQuiz[qnum].a){
+                console.log('right');
+                pkg.quiz.beaver.push(qnum);
+                if (pkg.quiz.beaver.length >= 3){
+                    expert();
+                } else if (qnum == 3){
+                    nicetry();
+                } else {
+                    qnum = qnum + 1;
+                }
+            } else {
+                console.log('wrong');
+                if (qnum == 3){
+                    nicetry();
+                } else {
+                    qnum = qnum + 1;
+                }
+            }
+        }
+        
+        if (pkg.animal == 3){
+            if (butnum == JellyfishQuiz[qnum].a){
+                console.log('right');
+                pkg.quiz.jellyfish.push(qnum);
+                if (pkg.quiz.jellyfish.length >= 3){
+                    expert();
+                } else if (qnum == 3){
+                    nicetry();
+                } else {
+                    qnum = qnum + 1;
+                }
+            } else {
+                console.log('wrong');
+                if (qnum == 3){
+                    nicetry();
+                } else {
+                    qnum = qnum + 1;
+                }
+            }
+        }
+        
+        if (pkg.animal == 4){
+            if (butnum == OrcaQuiz[qnum].a){
+                console.log('right');
+                pkg.quiz.orca.push(qnum);
+                if (pkg.quiz.orca.length >= 3){
+                    expert();
+                } else if (qnum == 3){
+                    nicetry();
+                } else {
+                    qnum = qnum + 1;
+                }
+            } else {
+                console.log('wrong');
+                if (qnum == 3){
+                    nicetry();
+                } else {
+                    qnum = qnum + 1;
+                }
             }
         }
     }
@@ -1022,14 +1624,17 @@ function checka (butnum){
     qna();
 }
 
-
+var qresult = document.querySelector(".result");
 
 function expert(){
     console.log("you're an expert");
-    
+    qresult.style.display = "block";
+    qresult.style.backgroundColor = "green";
 }
 
 function nicetry(){
     console.log('nice try');
+    qresult.style.display = "block";
+    qresult.style.backgroundColor = "red";
 }
 
